a2afe9ac5eda5175a9422390f1d9d2b906a103fe
Willkommen <turash@gmx.de>
2019-01-31 16:03:56 +0600

    post needs editing

--- resources/views/_includes/nav/manage.blade.php
+++ resources/views/_includes/nav/manage.blade.php
@@ -1,35 +1,4 @@
-<!--<div class="side-menu">-->
-<!--<div class="column is-narrow">
-  <div class="box" style="width: 200px; border: none; box-shadow: none; border-radius: 0px;">
-    <aside class="menu">
-        <p class="menu-label">
-          General
-        </p>
-        <ul class="menu-list">
-          <li><a href="{{route('manage.dashboard')}}">Dashboard</a></li>
-        </ul>
-        <p class="menu-label">
-          Administration
-        </p>
-        <ul class="menu-list">
-          <li>
-            <a href="{{route('users.index')}}">Manage Users</a>
-          </li>
-          <li>
-            <a  href="{{route('permissions.index')}}" class="is-active">Roles and Permissions</a>
-            <ul>
-              <li><a href="{{route('roles.index')}}">Roles</a></li>
-              <li><a href="{{route('permissions.index')}}">Permissions</a></li>
-            </ul>
-          </li>
-          <li><a>sample</a></li>
-        </ul>
-      </aside>
-  </div>
-</div>-->
-<!--</div>-->
 <div class="column is-narrow">
-<!--<div class="side-menu">-->
     <aside class="menu m-t-30 m-l-10">
       <p class="menu-label">
         General
@@ -37,7 +6,12 @@
       <ul class="menu-list">
       <li><a href="{{route('manage.dashboard')}}" class="{{Nav::isRoute('manage.dashboard')}}">Dashboard</a></li>
       </ul>
-  
+      <p class="menu-label">
+        Content
+      </p>
+      <ul class="menu-list">
+      <li><a href="{{route('posts.index')}}" class="{{Nav::isResource('post', 2)}}">Posts</a></li>
+      </ul>
       <p class="menu-label">
         Administration
       </p>
@@ -53,20 +27,19 @@
         <li>
           <a class="has-submenu">Example Accordion<span class="icon is-pulled-right"><i class="fa fa-caret-down"></i></span></a>
           <ul class="submenu">
-            <li><a href="{{route('roles.index')}}">Roles</a></li>
-            <li><a href="{{route('permissions.index')}}">Permissions</a></li>
+            <li><a href="#">Sub Menu 1</a></li>
+            <li><a href="#">Sub Menu 2</a></li>
           </ul>
         </li>
         <li>
           <a class="has-submenu">Another Example<span class="icon is-pulled-right"><i class="fa fa-caret-down"></i></span></a>
           <ul class="submenu">
-            <li><a href="{{route('roles.index')}}">Roles</a></li>
-            <li><a href="{{route('permissions.index')}}">Permissions</a></li>
+            <li><a href="#">Sub Menu 1</a></li>
+            <li><a href="#">Sub Menu 2</a></li>
           </ul>
         </li>
       </ul>
     </aside>
- <!-- </div>-->
 </div>
   
       
